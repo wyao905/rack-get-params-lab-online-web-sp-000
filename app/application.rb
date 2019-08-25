420e11713abfc816faf053863ca2a8bec63efafe
Warren Yao <warrenyca905@gmail.com>
2019-08-25 16:30:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,9 +15,12 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if @@cart.size
-      @@cart.each do |item|
-        resp.write "#{item}\n"
+      if @@cart.size == 0
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
       end
     elsif req.path.match(/add/)
       add_item = req.params["item"]
